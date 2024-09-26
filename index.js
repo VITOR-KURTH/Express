012a311661a84a100531b05682a753f2d4b72356
VÍTOR KURTH VASCONCELLOS FERREIRA <vitor_k_ferreira@estudante.sesisenai.org.br>
2024-09-26 09:59:05 -0300

    Express

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { url } = require('inspector');
+const app = express()
+const port = 3000 //variavel ambiente
+
+const path = require("path")
+const basepath = path.join(__dirname, 'Aula1')
+const checkAuth = function (req, res, next) {
+    req.authStatus = true
+    if (req.authStatus) {
+        console.log('Está logado, pode continuar')
+    } else {
+        console.log('Não está logado, faça o login!')
+    }
+}
+
+app.use(checkAuth)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basepath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App está rodando na porta ${port}`)
+
+})
